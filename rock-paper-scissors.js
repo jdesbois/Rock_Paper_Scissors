7fef4983277b9a4335b73c71b193dc04650e683c
John Desbois <jdesbois@me.com>
2018-10-28 19:16:09 +0000

    Random Computer pick and one round of game complete (commented)

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,8 +1,9 @@
-function computerPlay () {
+//Function to pick random number, uses number to link to R P or S
+function computerPlay () { //generates random number between 1-3
     var result = Math.floor((Math.random() * 3) + 1);
     var computerPick;
 
-    switch(result) {
+    switch(result) { //swtich statement returns computers pick from random number generated.
         case 1:
         return computerPick = "Rock";
         case 2:
@@ -13,13 +14,31 @@ function computerPlay () {
     return computerPick;
 }
 
+
 function rockPaperScissors (playerSelection, computerSelection) {
-    playerSelection = prompt("Rock, Paper or Scissors?");
-    computerSelection = computerPlay();
+    playerSelection = prompt("Rock, Paper or Scissors?"); //Prompts for Player input
+    computerSelection = computerPlay(); //runs computerPlay function and returns computers random choice
 
-    playerSelectionSplit = playerSelection.split("");
-    computerSelectionSplit = computerSelection.split("");
+    playerSelectionClean = playerSelection.toLowerCase().slice(0,1); //Lowercase and slices first character
+    computerSelectionClean = computerSelection.toLowerCase().slice(0,1); //Lowercase and slices first character
 
+    WinnerWinnerChickenDinner = ""; //Winner of this round variable 
+    console.log(playerSelection + "*********" + computerSelection); //output of choices (debugging)
+
+    if (playerSelectionClean == computerSelectionClean) { //checks for a draw scenario
+        return WinnerWinnerChickenDinner = "Draw! You both picked " + playerSelection + " Play again";
+    }
+
+    // checks each Player win scenario if all fail then default computer win scenario
+    if (playerSelectionClean == "r" && computerSelectionClean == "s") {
+        WinnerWinnerChickenDinner = "You Win! " + playerSelection + " beats " + computerSelection;
+    } else if (playerSelectionClean == "s" && computerSelectionClean == "p") {
+        WinnerWinnerChickenDinner = "You win! " + playerSelection + " beats " + computerSelection;
+    } else if (playerSelectionClean == "p" && computerSelectionClean == "r") {
+        WinnerWinnerChickenDinner = "You win! " + playerSelection + " beats " + computerSelection;
+    } else {
+        WinnerWinnerChickenDinner = "Computer Wins! " + computerSelection + " beats " + playerSelection;
+    }
     
-    
+    return WinnerWinnerChickenDinner; //returns outcome of round
 }
