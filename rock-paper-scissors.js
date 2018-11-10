a6d1cd3a5e39bc6ab00c8d0b673cd3687eda2ff4
John Desbois <jdesbois@me.com>
2018-11-10 19:08:40 +0000

    Updated UI and Logic

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,14 @@
+var listItem = document.getElementsByTagName('li');
+var playerScore = 0;
+var computerScore = 0;
+var playerScoreElement = document.getElementById("playerScore");
+var computerScoreElement = document.getElementById("computerScore");
+computerScoreElement.innerHTML = computerScore;
+playerScoreElement.innerHTML = playerScore;
+
+for (var i=0; i < listItem.length; i++) {
+    listItem[i].addEventListener("click", rockPaperScissors, false);
+}
 //Function to pick random number, uses number to link to R P or S
 function computerPlay () { //generates random number between 1-3
     var result = Math.floor((Math.random() * 3) + 1);
@@ -16,37 +27,51 @@ function computerPlay () { //generates random number between 1-3
 
 
 function rockPaperScissors (playerSelection, computerSelection) {
+    
     var resultOutput = document.getElementById("result");
-    playerSelection = prompt("Rock, Paper or Scissors?");
+    playerSelection = this.innerHTML;
     computerSelection = computerPlay(); //runs computerPlay function and returns computers random choice
 
     playerSelectionClean = playerSelection.toLowerCase().slice(0,1); //Lowercase and slices first character
     computerSelectionClean = computerSelection.toLowerCase().slice(0,1); //Lowercase and slices first character
-
+    console.log(playerSelection + computerSelection);
     WinnerWinnerChickenDinner = ""; //Winner of this round variable 
     // console.log(playerSelection + "*********" + computerSelection); //output of choices (debugging)
 
-    if (playerSelectionClean == computerSelectionClean) { //checks for a draw scenario
-        return WinnerWinnerChickenDinner = "Draw! You both picked " + playerSelection + " Play again";
-    }
+    
 
     // checks each Player win scenario if all fail then default computer win scenario
-    if (playerSelectionClean == "r" && computerSelectionClean == "s") {
-        WinnerWinnerChickenDinner = "You Win! " + playerSelection + " beats " + computerSelection;
+    if (playerSelectionClean == computerSelectionClean) { //checks for a draw scenario
+        WinnerWinnerChickenDinner = "Draw! You both picked " + playerSelection + ", Play again?";
+        playerScore += 1;
+        playerScoreElement.innerHTML = playerScore;
+        computerScore += 1;
+        computerScoreElement.innerHTML = computerScore;
+    } else if (playerSelectionClean == "r" && computerSelectionClean == "s") {
+        WinnerWinnerChickenDinner = "You win! " + playerSelection + " beats " + computerSelection;
+        playerScore += 1;
+        playerScoreElement.innerHTML = playerScore;
     } else if (playerSelectionClean == "s" && computerSelectionClean == "p") {
         WinnerWinnerChickenDinner = "You win! " + playerSelection + " beats " + computerSelection;
+        playerScore += 1;
+        playerScoreElement.innerHTML = playerScore;
     } else if (playerSelectionClean == "p" && computerSelectionClean == "r") {
         WinnerWinnerChickenDinner = "You win! " + playerSelection + " beats " + computerSelection;
+        playerScore += 1;
+        playerScoreElement.innerHTML = playerScore;
     } else {
-        WinnerWinnerChickenDinner = "Computer Wins! " + computerSelection + " beats " + playerSelection;
+        WinnerWinnerChickenDinner = "Computer wins! " + computerSelection + " beats " + playerSelection;
+        computerScore += 1;
+        computerScoreElement.innerHTML = computerScore;
     }
     
-    resultOutput.innerHTML = WinnerWinnerChickenDinner;
-    // console.log(WinnerWinnerChickenDinner); //returns outcome of round
+    
+    resultOutput.innerHTML = WinnerWinnerChickenDinner; //outputs result to the page.
+    playerSelection = "";
+    computerSelection = "";
+    // console.log(WinnerWinnerChickenDinner); //returns outcome of round to console
 
 }
 
-var listItem = document.getElementsByTagName('li')
-for (var i=0; i < listItem.length; i++) {
-    listItem[i].addEventListener("click", rockPaperScissors, false);
-}
+
+
